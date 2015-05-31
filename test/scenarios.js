2a8f9f6efbe75c06e89f0cbe855ae5a7428911f3
Greg MacWilliam <gmacwill77@gmail.com>
2015-05-31 08:32:58 -0400

    Adding other tests to fulfill.

---
+++ test/scenarios.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var assert = require('assert');
+var fileImporter = require('../index');
+
+describe('other import scenarios', function() {
+  it ('errors upon encountering recursive imports.', function(done) {
+    
+  });
+
+  it ('ignores url() imports.', function(done) {
+    
+  });
+
+  it ('ignores absolute imports.', function(done) {
+    
+  });
+]);
